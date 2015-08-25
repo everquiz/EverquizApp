ce0147fff4ac44c1f6fdcb5c4e43e59037a2a113
goldman <lelou>
2015-08-25 18:44:20 +0300

    created skeleton for project v0.1

--- app.js
+++ app.js
@@ -1,6 +1,66 @@
 var express = require('express');
+var path = require('path');
+var bodyParser    = require('body-parser');
+
+
+
+/*
+  Connect to db
+ */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+
+var post = require('./models/Posts');
+require('./models/Comments');
+
+/*
+  Routes
+ */
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(1337);
+app.use('/', routes);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 var mongoose = require('../lib/mongoose');
    
-exports.User = mongoose.model('User', { name: String,
-                                    mail: String,
-                                    password: String,
-                                    status: String,
-                                    notes: Array,
-                                    history: Array,
-                                    statistics: Array,
-                                    follows: Array
-                                  });
-  
+exports.User = mongoose.model('User', 
+  { name: String,
+    mail: String,
+    password: String,
+    status: String,
+    notes: Array,
+    history: Array,
+    statistics: Array,
+    follows: Array
+  });
