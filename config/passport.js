ff1c6725bb0ddeb87cad59bcccdfc10069aadeae
Oleksii Kukuruza <oleksii.kukuruza@gmail.com>
2015-08-30 12:52:10 +0300

    Deleted log

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,6 @@ passport.use(new LocalStrategy({
   },
   function(email, password, done) {
     User.findOne({ email: email }, function (err, user) {
-      console.log('hi');
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect email.' });
