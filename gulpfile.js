dbe919e1e8f9b1b24c30f2d58fb5b091e0de0c32
AndrewLolo <andrewloloal@gmail.com>
2015-08-31 19:50:55 +0300

    Added gulp minify-js and minify-css tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,34 @@ var mainBowerFiles = require('main-bower-files');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var addsrc = require('gulp-add-src');
+var sass = require('gulp-sass');
 
-gulp.task('angularJS', function() {
+gulp.task('minify-js', function() {
     return gulp.src(mainBowerFiles('**/*.js'))
-        .pipe(gulp.dest('public/dist/js'))
+        .pipe(addsrc.append([
+            'assets/*.js',
+            'assets/angular-js/*.js',
+            'assets/angular-js/services/*.js',
+            'assets/angular-js/controllers/*.js',
+            'assets/angular-js/config/*.js',
+            'assets/generic-js/*.js']))
+        .pipe(concat('application.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('minify-css', function() {
-    return gulp.src('assets/styles/*.css')
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('public/dist/css'));
-});
-
-gulp.task('normalizeCSS', function() {
     return gulp.src(mainBowerFiles('**/*.css'))
+        .pipe(addsrc.append([
+            'assets/styles/*.scss',
+            'assets/styles/*.css']))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('application.min.css'))
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('assets/styles'))
+        .pipe(gulp.dest('public'));
 });
 
-gulp.task('compress', function() {
-    return gulp.src('assets/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('public/dist/js'));
-});
-
-gulp.task('default', function() {
-        gulp.run('angularJS');
-        gulp.run('normalizeCSS')
-        gulp.run('minify-css');
-        gulp.run('compress');
+gulp.task('default', ['minify-css', 'minify-js'], function() {
     }
 );
