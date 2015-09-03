aa22f0823c469d48564bb6f06d6a3135a8e9100f
AndrewLolo <andrewloloal@gmail.com>
2015-09-03 13:02:40 +0300

    Gulp update: CSS separation tasks included

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,55 @@ var sass = require('gulp-sass');
 var exec = require('child_process').exec;
 var ngAnnotate = require('gulp-ng-annotate');
 
+//****************************************************************
+//JavaScripts section
 gulp.task('minify-js', function() {
     return gulp.src(mainBowerFiles('**/*.js'))
         .pipe(addsrc.append([
             'assets/*.js',
             'assets/angular-js/*.js',
             'assets/angular-js/config/*.js',
-
             'assets/angular-js/services/*.js',
             'assets/angular-js/controllers/*.js',
             'assets/generic-js/*.js']))
         .pipe(concat('application.min.js'))
         .pipe(ngAnnotate())
         .pipe(uglify())
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('minify-css', function() {
+
+//****************************************************************
+//CSS section
+//External styles
+gulp.task('normalize', function() {
     return gulp.src(mainBowerFiles('**/*.css'))
-        .pipe(addsrc.append([
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public/css/vendor'));
+});
+
+gulp.task('vendor-css', ['normalize'], function(){
+});
+
+//Custom styles
+gulp.task('custom-css', function() {
+    return gulp.src([
             'assets/styles/*.scss',
-            'assets/styles/*.css']))
+            'assets/styles/*.css'])
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('application.min.css'))
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('public/css/custom'));
 });
 
-gulp.task('server', ['minify-css', 'minify-js'], function (cb) {
+//Prepare all styles
+gulp.task('general-css', ['vendor-css', 'custom-css'], function() {
+});
+
+//****************************************************************
+//General section
+//Run server
+gulp.task('server', ['general-css', 'minify-js'], function (cb) {
   exec('npm start', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -43,7 +64,7 @@ gulp.task('server', ['minify-css', 'minify-js'], function (cb) {
   });
 });
 
+//Default task
 gulp.task('default', ['server'], function() {
-
 });
 
