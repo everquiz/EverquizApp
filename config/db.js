9ae05578388894037216bfb0648a11f297cd5ac5
wbdrd <thewbdrd@gmail.com>
2015-09-17 23:39:40 +0300

    Remake markup for home page + make CSS layout for all views

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-// mongoose.connect('mongodb://root:root@ds041663.mongolab.com:41663/everquizdb');
+//mongoose.connect('mongodb://root:root@ds041663.mongolab.com:41663/everquizdb');
 mongoose.connect('mongodb://localhost/everquizdb');
 
 db.on('connected', function() {
