130eda130963dd86f1606abdb625c97c9d871d5a
Oleksii Kukuruza <oleksii.kukuruza@gmail.com>
2015-09-21 14:26:05 +0300

    added historyService

--- app/angular/quizzes/list/RunQuizzesController.js
+++ app/angular/quizzes/list/RunQuizzesController.js
@@ -1,88 +1,93 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-      .module('everquizApp')
-      .controller('RunQuizzesController', RunQuizzesController);
+    angular
+        .module('everquizApp')
+        .controller('RunQuizzesController', RunQuizzesController);
 
-    RunQuizzesController.$inject = ['quizzes', 'quizService', 'categoryService', '$scope'];
+    RunQuizzesController.$inject = ['quizzes', 'quizService', 'categoryService', '$scope', 'history'];
 
-  function RunQuizzesController(quizzes, quizService, categoryService, $scope) {
-    var vm = this;
-    vm.selectedCategory = -1;
-    vm.selectedComplexity = -1;
-    vm.quizzes = quizzes;
-    vm.updateQuizzes = updateQuizzes;
-    vm.difficulties = [
-        {_id: -1, title: 'All difficulties'},
-        {_id: 0, title: 'Novice'},
-        {_id: 1, title: 'Advanced'},
-        {_id: 2, title: 'Expert'}
-      ];
-    vm.getComplexity = getComplexity;
-    vm.updateFilteredQuizzes = updateFilteredQuizzes;
-    categoryService.getCategories().then(function(data) {
-      vm.categories = data;
-      vm.categories.unshift({_id: -1, title: 'All categories'})
-    });
+    function RunQuizzesController(quizzes, quizService, categoryService, $scope, history) {
+        var vm = this;
+        vm.selectedCategory = -1;
+        vm.selectedComplexity = -1;
+        vm.quizzes = quizzes;
+        vm.updateQuizzes = updateQuizzes;
+        vm.history = history;
+        vm.difficulties = [
+            {_id: -1, title: 'All difficulties'},
+            {_id: 0, title: 'Novice'},
+            {_id: 1, title: 'Advanced'},
+            {_id: 2, title: 'Expert'}
+        ];
+        vm.getComplexity = getComplexity;
+        vm.updateFilteredQuizzes = updateFilteredQuizzes;
+        categoryService.getCategories().then(function (data) {
+            vm.categories = data;
+            vm.categories.unshift({_id: -1, title: 'All categories'})
+        });
 
-    /**
-     * Paginations variables
-     */
-    vm.maxSize = $scope.maxSize = 5;
-    vm.numPerPage = $scope.numPerPage = 5;
-    $scope.currentPage = $scope.currentPage = 1;
-    vm.numPages = numPages;
-    quizService.getAllTest().then(function(res) {
-      vm.filteredQuizzes = res.slice(0, vm.numPerPage);
-    });
+        /**
+         * Paginations variables
+         */
+        vm.maxSize = $scope.maxSize = 5;
+        vm.numPerPage = $scope.numPerPage = 5;
+        $scope.currentPage = $scope.currentPage = 1;
+        vm.numPages = numPages;
+        quizService.getAllTest().then(function (res) {
+            vm.filteredQuizzes = res.slice(0, vm.numPerPage);
+        });
 
-    function updateQuizzes() {
-      var category, complexity, status, query;
-      console.log(vm.selectedCategory);
-      if (vm.selectedCategory === -1) {
-        category = '!=-11111111111111111111111';
-      } else {
-        category = vm.selectedCategory;
-      };
-      if (vm.selectedComplexity === -1) {
-        complexity = '!=1'
-      } else {
-        complexity = vm.selectedComplexity;
-      };
+        function updateQuizzes() {
+            var category, complexity, status, query;
+            console.log(vm.selectedCategory);
+            if (vm.selectedCategory === -1) {
+                category = '!=-11111111111111111111111';
+            } else {
+                category = vm.selectedCategory;
+            }
+            ;
+            if (vm.selectedComplexity === -1) {
+                complexity = '!=1'
+            } else {
+                complexity = vm.selectedComplexity;
+            }
+            ;
 
-      query = 'category=' + category + '&complexity=' + complexity;
-      quizService.getQuizzesByQuery(query).then(function (data) {
-        vm.quizzes = data;
-        vm.updateFilteredQuizzes();
-      });
-    }
+            query = 'category=' + category + '&complexity=' + complexity;
+            quizService.getQuizzesByQuery(query).then(function (data) {
+                vm.quizzes = data;
+                vm.updateFilteredQuizzes();
+            });
+        }
 
-    function getComplexity(complexity) {
-      for (var i = vm.difficulties.length - 1; i >= 0; i--) {
-        if (vm.difficulties[i]._id === complexity) {
-          return vm.difficulties[i].title;
+        function getComplexity(complexity) {
+            for (var i = vm.difficulties.length - 1; i >= 0; i--) {
+                if (vm.difficulties[i]._id === complexity) {
+                    return vm.difficulties[i].title;
+                }
+                ;
+            }
+            ;
         };
-      };
-    };
 
-    function numPages() {
-      return Math.ceil(vm.quizzes.length / vm.numPerPage);
-    };
+        function numPages() {
+            return Math.ceil(vm.quizzes.length / vm.numPerPage);
+        };
 
-    /**
-     * Watcher for paggination
-     */
-    $scope.$watch('currentPage + numPerPage' , updateFilteredQuizzes);
+        /**
+         * Watcher for paggination
+         */
+        $scope.$watch('currentPage + numPerPage', updateFilteredQuizzes);
 
-    /**
-     * Handler for pagination
-     */
-    function updateFilteredQuizzes () {
-      var begin = (($scope.currentPage - 1) * vm.numPerPage)
-      , end = begin + vm.numPerPage;
-      vm.filteredQuizzes = vm.quizzes.slice(begin, end);
-      console.log(vm.filteredQuizzes);
+        /**
+         * Handler for pagination
+         */
+        function updateFilteredQuizzes() {
+            var begin = (($scope.currentPage - 1) * vm.numPerPage)
+                , end = begin + vm.numPerPage;
+            vm.filteredQuizzes = vm.quizzes.slice(begin, end);
+            console.log(vm.filteredQuizzes);
+        }
     }
-  }
 })();
--- app/angular/routes.js
+++ app/angular/routes.js
@@ -1,182 +1,189 @@
-(function() {
-  'use strict'
-  angular
-      .module('everquizApp')
-      .config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
+(function () {
+    'use strict'
+    angular
+        .module('everquizApp')
+        .config([
+            '$stateProvider',
+            '$urlRouterProvider',
+            function ($stateProvider, $urlRouterProvider) {
 
-          // Handler for Restricting Access to a page using the state.resolve call
-          var accessUser = function (authFactory, $state) {
-            if (!authFactory.isLoggedIn()) {
-              alert('You have to login');
-              $state.go('/login');
-            }
-            ;
-          };
+                // Handler for Restricting Access to a page using the state.resolve call
+                var accessUser = function (authFactory, $state) {
+                    if (!authFactory.isLoggedIn()) {
+                        alert('You have to login');
+                        $state.go('/login');
+                    }
+                    ;
+                };
 
-          var accessAdmin = function (authFactory, $state) {
-            if (!authFactory.isAdmin()) {
-              alert('You have to be admin to be here');
-              $state.go('/login');
-            }
-            ;
-          };
+                var accessAdmin = function (authFactory, $state) {
+                    if (!authFactory.isAdmin()) {
+                        alert('You have to be admin to be here');
+                        $state.go('/login');
+                    }
+                    ;
+                };
 
-          $stateProvider
-              .state('home', {
-                url: '/',
-                views: {
-                  '': {
-                    templateUrl: 'home/index.html'
-                  },
-                  'header@home': {
-                    templateUrl: 'layouts/_header.html',
-                    controller: 'NavController as NavCtrl'
-                  },
-                  'profile@home': {
-                    templateUrl: 'profile/_profile.html',
-                    controller: 'ProfileController as ProfileCtrl',
-                    resolve: {
-                      profile: [ 'profileFactory',
-                      function (profileFactory) {
-                        return profileFactory.updateProfile();
-                      }]}
-                  },
-                  'getStarted@home': {
-                    templateUrl: 'home/_getStarted.html'
-                  },
-                  'video@home': {
-                    templateUrl: 'video/_video.html'
-                  },
-                  'quizzes@home': {
-                    templateUrl: 'quizzes/_quizzes.html',
-                    controller: "QuizzesContainerController as QuizzesContainerCtrl"
-                  },
-                  'list@home': {
-                    templateUrl: 'quizzes/list/_list.html',
-                    controller: 'RunQuizzesController as RunQuizzesCtrl',
-                    resolve: {
-                      quizzes: [ 'quizService', 
-                        function (quizService) {
-                          return quizService.getQuizzes();
+                $stateProvider
+                    .state('home', {
+                        url: '/',
+                        views: {
+                            '': {
+                                templateUrl: 'home/index.html'
+                            },
+                            'header@home': {
+                                templateUrl: 'layouts/_header.html',
+                                controller: 'NavController as NavCtrl'
+                            },
+                            'profile@home': {
+                                templateUrl: 'profile/_profile.html',
+                                controller: 'ProfileController as ProfileCtrl',
+                                resolve: {
+                                    profile: ['profileFactory',
+                                        function (profileFactory) {
+                                            return profileFactory.updateProfile();
+                                        }]
+                                }
+                            },
+                            'getStarted@home': {
+                                templateUrl: 'home/_getStarted.html'
+                            },
+                            'video@home': {
+                                templateUrl: 'video/_video.html'
+                            },
+                            'quizzes@home': {
+                                templateUrl: 'quizzes/_quizzes.html',
+                                controller: "QuizzesContainerController as QuizzesContainerCtrl"
+                            },
+                            'list@home': {
+                                templateUrl: 'quizzes/list/_list.html',
+                                controller: 'RunQuizzesController as RunQuizzesCtrl',
+                                resolve: {
+                                    quizzes: ['quizService',
+                                        function (quizService) {
+                                            return quizService.getQuizzes();
+                                        }],
+                                    history: ['historyService',
+                                        function (historyService) {
+                                            return historyService.getHistory();
+                                        }]
+                                }
+                            },
+                            'quiz@home': {
+                                templateUrl: 'quizzes/single/_quiz.html',
+                                controller: 'PassingQuizController as PassingQuizCtrl'
+                            },
+                            'notes@home': {
+                                templateUrl: 'notes/_notes.html',
+                                controller: 'NotesContainerController as NotesContainerCtrl'
+                            },
+                            'notesList@home': {
+                                templateUrl: 'notes/_notes.list.html',
+                                controller: 'NotesListController as NotesListCtrl',
+                                resolve: {
+                                    notes: ['notesService',
+                                        function (notesService) {
+                                            return notesService.getNotes();
+                                        }]
+                                }
+                            },
+                            'notesMain@home': {
+                                templateUrl: 'notes/_notes.main.html',
+                                controller: 'NotesMainController as NotesMainCtrl',
+                                resolve: {
+                                    notes: ['notesService',
+                                        function (notesService) {
+                                            return notesService.getNotes();
+                                        }]
+                                }
+                            },
+                            'getFail@home': {
+                                templateUrl: 'home/_getFail.html'
+                            },
+                            'footer@home': {
+                                templateUrl: 'layouts/_footer.html'
+                            },
+                            'addQuiz@home': {
+                                templateUrl: 'quizzes/_addQuiz.html'
+                            }
+                        }
+                    })
+                    .state('admin', {
+                        url: '/admin',
+                        templateUrl: 'admin/index.html',
+                        resolve: {loginRequired: accessAdmin}
+                    })
+                    .state('admin.categories', {
+                        url: '/categories',
+                        templateUrl: 'admin/categories/_categories.html',
+                        controller: 'CategoriesController as CategoriesCtrl',
+                        resolve: {
+                            categories: ['categoryService',
+                                function (categoryService) {
+                                    return categoryService.getAll();
+                                }]
+                        }
+                    })
+                    .state('admin.quizzes', {
+                        url: '/quizzes',
+                        templateUrl: 'admin/quizzes/_quizzes.html',
+                        controller: 'QuizzesController as QuizzesCtrl',
+                        resolve: {
+                            quizzes: ['quizService',
+                                function (quizService) {
+                                    return quizService.getAll();
+                                }]
+                        }
+                    })
+                    .state('admin.quiz', {
+                        url: '/quiz/{id}',
+                        templateUrl: 'admin/quiz/_quiz.html',
+                        controller: 'QuizController as QuizCtrl',
+                        resolve: {
+                            quiz: ['$stateParams', 'quizService',
+                                function ($stateParams, quizService) {
+                                    return quizService.get($stateParams.id);
+                                }]
+                        }
+                    })
+                    .state('admin.question', {
+                        url: '/question/{id}',
+                        templateUrl: 'admin/question/_question.html',
+                        controller: 'QuestionController as QuestionCtrl',
+                        resolve: {
+                            question: ['$stateParams', 'questionService',
+                                function ($stateParams, questionService) {
+                                    return questionService.get($stateParams.id);
+                                }]
+                        }
+                    })
+                    .state('admin.users', {
+                        url: '/users',
+                        templateUrl: 'admin/users/_users.html',
+                        controller: 'UsersController as UsersCtrl',
+                        resolve: {
+                            userPromise: ['userService',
+                                function (userService) {
+                                    return userService.getAll();
+                                }]
+                        }
+                    })
+                    .state('login', {
+                        url: '/login',
+                        templateUrl: 'auth/login.html',
+                        controller: 'AuthController as AuthCtrl'
+                    })
+                    .state('register', {
+                        url: '/register',
+                        templateUrl: 'auth/register.html',
+                        controller: 'AuthController as AuthCtrl',
+                        onEnter: ['$state', 'authFactory', function ($state, authFactory) {
+                            if (authFactory.isLoggedIn()) {
+                                $state.go('home');
+                            }
                         }]
-                      }
-                  },
-                  'quiz@home': {
-                    templateUrl: 'quizzes/single/_quiz.html',
-                    controller: 'PassingQuizController as PassingQuizCtrl'
-                  },
-                  'notes@home': {
-                    templateUrl: 'notes/_notes.html',
-                    controller: 'NotesContainerController as NotesContainerCtrl'
-                  },
-                  'notesList@home': {
-                    templateUrl: 'notes/_notes.list.html',
-                    controller: 'NotesListController as NotesListCtrl',
-                    resolve: {
-                      notes: [ 'notesService',
-                        function (notesService) {
-                          return notesService.getNotes();
-                        }]}
-                  },
-                  'notesMain@home': {
-                    templateUrl: 'notes/_notes.main.html',
-                    controller: 'NotesMainController as NotesMainCtrl',
-                    resolve: {
-                      notes: [ 'notesService',
-                        function (notesService) {
-                          return notesService.getNotes();
-                        }]}
-                  },
-                  'getFail@home': {
-                    templateUrl: 'home/_getFail.html'
-                  },
-                  'footer@home': {
-                    templateUrl: 'layouts/_footer.html'
-                  },
-                  'addQuiz@home': {
-                    templateUrl: 'quizzes/_addQuiz.html'
-                  }
-                }
-              })
-              .state('admin', {
-                url: '/admin',
-                templateUrl: 'admin/index.html',
-                resolve: {loginRequired: accessAdmin}
-              })
-              .state('admin.categories', {
-                url: '/categories',
-                templateUrl: 'admin/categories/_categories.html',
-                controller: 'CategoriesController as CategoriesCtrl',
-                resolve: {
-                  categories: ['categoryService',
-                    function (categoryService) {
-                      return categoryService.getAll();
-                    }]
-                }
-              })
-              .state('admin.quizzes', {
-                url: '/quizzes',
-                templateUrl: 'admin/quizzes/_quizzes.html',
-                controller: 'QuizzesController as QuizzesCtrl',
-                resolve: {
-                  quizzes: ['quizService',
-                    function (quizService) {
-                      return quizService.getAll();
-                    }]
-                }
-              })
-              .state('admin.quiz', {
-                url: '/quiz/{id}',
-                templateUrl: 'admin/quiz/_quiz.html',
-                controller: 'QuizController as QuizCtrl',
-                resolve: {
-                  quiz: ['$stateParams', 'quizService',
-                    function ($stateParams, quizService) {
-                      return quizService.get($stateParams.id);
-                    }]
-                }
-              })
-              .state('admin.question', {
-                url: '/question/{id}',
-                templateUrl: 'admin/question/_question.html',
-                controller: 'QuestionController as QuestionCtrl',
-                resolve: {
-                  question: ['$stateParams', 'questionService',
-                    function ($stateParams, questionService) {
-                      return questionService.get($stateParams.id);
-                    }]
-                }
-              })
-              .state('admin.users', {
-                url: '/users',
-                templateUrl: 'admin/users/_users.html',
-                controller: 'UsersController as UsersCtrl',
-                resolve: {
-                  userPromise: ['userService',
-                    function (userService) {
-                      return userService.getAll();
-                    }]
-                }
-              })
-              .state('login', {
-                url: '/login',
-                templateUrl: 'auth/login.html',
-                controller: 'AuthController as AuthCtrl'
-              })
-              .state('register', {
-                url: '/register',
-                templateUrl: 'auth/register.html',
-                controller: 'AuthController as AuthCtrl',
-                onEnter: ['$state', 'authFactory', function ($state, authFactory) {
-                  if (authFactory.isLoggedIn()) {
-                    $state.go('home');
-                  }
-                }]
-              });
-          $urlRouterProvider.otherwise('/');
-        }]);
+                    });
+                $urlRouterProvider.otherwise('/');
+            }]);
 
 })();
