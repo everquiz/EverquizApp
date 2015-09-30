7cf3141fddaf3a58f7d426b84b9833bea60fd23b
AndrewLolo <andrewloloal@gmail.com>
2015-10-01 01:33:09 +0300

    pagination directive added

--- app/features/paginationDirective.js
+++ app/features/paginationDirective.js
@@ -7,37 +7,78 @@
 
         function eqPagination() {
             return {
-                template: "<ul><li>First page</li><li>Previous page</li><li ng-repeat='page in pageList'>{{page}}</li><li>Next page</li><li>Last page</li></ul>",
+                template: '<div class="notes-list-pagination">' +
+                '<ul class="notes-list-pagination-buttonset">' +
+                '<li class="notes-list-pagination-buttonset-item" ng-click="goToFirstPage()"> <span class="notes-list-pagination-buttonset-item__title is-first">First</span> </li> ' +
+                '<li class="notes-list-pagination-buttonset-item" ng-click="goToPreviousPage()"> <span class="notes-list-pagination-buttonset-item__title">&laquo;</span> </li> ' +
+                '<li class="notes-list-pagination-buttonset-item" ng-click="goToPage(page)" ng-repeat="page in pageList" ' +
+                'ng-class=' + '"{' +
+                "'is-active'" +
+                ': activePage === page}"> ' +
+                '<span class="notes-list-pagination-buttonset-item__title">{{page + 1}}</span></li>' +
+                '<li class="notes-list-pagination-buttonset-item" ng-click="goToNextPage()"><span class="notes-list-pagination-buttonset-item__title">&raquo;</span></li>' +
+                '<li class="notes-list-pagination-buttonset-item" ng-click="goToLastPage()"> <span class="notes-list-pagination-buttonset-item__title is-last">Last</span> </li> ' +
+                '</ul> ' +
+                '</div>',
                 restrict: 'E',
                 replace: true,
                 scope: {
-                    elemList: '=',
-                    limit: '@',
-                    pageNum: '@',
-                    pageList: '@'
+                    viewList: '=',
+                    limit: '=',
+                    filter: '='
                 },
-                controller: function($scope) {
+                link: function (scope, element, attributes) {
 
-                },
-                link: function ($scope, element, attributes) {
-                    var activePage = 1;
-                    $scope.pageNum = 1;
-                    $scope.pageList = [];
+                    //Methods
+                    scope.goToFirstPage = goToFirstPage;
+                    scope.goToLastPage = goToLastPage;
+                    scope.goToNextPage = goToNextPage;
+                    scope.goToPreviousPage = goToPreviousPage;
+                    scope.goToPage = goToPage;
+                    scope.$parent.pageFilter = pageFilter;
 
-                    $scope.$watch("elemList", function(value) {
-                        if (value === undefined) return;
+                        //Properties
+                    scope.activePage = 0;
+                    scope.pageList = [];
+                    scope.numPages = 0;
 
-                        $scope.pageNum = (value.length / $scope.limit) >> 0;
-                        if (value.length % $scope.limit !== 0) $scope.pageNum++;
-
-                        $scope.pageList = [];
-                        for (var i = 0; i < $scope.pageNum - 1; ++i) $scope.pageList[i] = i + 1;
-                        console.log($scope.pageList);
+                    //Watchers
+                    scope.$watch('viewList.length',function(value){
+                        scope.numPages = (value / scope.limit) >> 0;
+                        if (value % scope.limit !== 0) scope.numPages++;
+                        scope.pageList.length = scope.numPages;
+                        for (var i = 0; i < scope.numPages; ++i) scope.pageList[i] = i;
                     });
 
-                    element.bind('click', function () {
-                        console.log($scope.elemList.length);
-                    });
+                    //Implementation
+                    function goToFirstPage() {
+                        scope.activePage = 0;
+                        console.log(scope.activePage);
+                    };
+
+                    function goToLastPage() {
+                        scope.activePage = scope.numPages - 1;
+                        console.log(scope.activePage);
+                    };
+
+                    function goToNextPage() {
+                        if (scope.activePage < scope.numPages - 1) scope.activePage++;
+                        console.log(scope.activePage);
+                    };
+
+                    function goToPreviousPage() {
+                        if (scope.activePage > 0) scope.activePage--;
+                        console.log(scope.activePage);
+                    };
+                    function goToPage(num) {
+                        scope.activePage = num;
+                        console.log(scope.activePage);
+                    };
+
+                    function pageFilter(index) {
+                        return ( (scope.activePage * scope.limit <= index) &&
+                        (index < scope.activePage * scope.limit + scope.limit));
+                    }
                 }
 
             }
