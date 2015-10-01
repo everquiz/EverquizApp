2cbcefaa35fe8bbe98c0f4fb59e9f00d3b337893
Viktor <valakar@gmail.com>
2015-10-01 23:14:41 +0300

    achievments restify

--- server/models/Achievements.js
+++ server/models/Achievements.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-var AchievementsSchema = new mongoose.Schema({
+var AchievementSchema = new mongoose.Schema({
     description: String,
     picture: { data: Buffer, contentType: String },
     title: String
 });
 
-module.exports = mongoose.model('Achievements', AchievementsSchema);
-module.exports.AchievementsSchema = AchievementsSchema;
+module.exports = mongoose.model('Achievement', AchievementSchema);
+module.exports.AchievementSchema = AchievementSchema;
