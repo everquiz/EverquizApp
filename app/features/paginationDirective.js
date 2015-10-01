6a7e40140046318ed1db29b9cd900f2dc7041fb3
AndrewLolo <andrewloloal@gmail.com>
2015-10-01 13:02:33 +0300

    Pagination update

--- app/features/paginationDirective.js
+++ app/features/paginationDirective.js
@@ -7,19 +7,7 @@
 
         function eqPagination() {
             return {
-                template: '<div class="notes-list-pagination">' +
-                '<ul class="notes-list-pagination-buttonset">' +
-                '<li class="notes-list-pagination-buttonset-item" ng-click="goToFirstPage()"> <span class="notes-list-pagination-buttonset-item__title is-first">First</span> </li> ' +
-                '<li class="notes-list-pagination-buttonset-item" ng-click="goToPreviousPage()"> <span class="notes-list-pagination-buttonset-item__title">&laquo;</span> </li> ' +
-                '<li class="notes-list-pagination-buttonset-item" ng-click="goToPage(page)" ng-repeat="page in pageList" ' +
-                'ng-class=' + '"{' +
-                "'is-active'" +
-                ': activePage === page}"> ' +
-                '<span class="notes-list-pagination-buttonset-item__title">{{page + 1}}</span></li>' +
-                '<li class="notes-list-pagination-buttonset-item" ng-click="goToNextPage()"><span class="notes-list-pagination-buttonset-item__title">&raquo;</span></li>' +
-                '<li class="notes-list-pagination-buttonset-item" ng-click="goToLastPage()"> <span class="notes-list-pagination-buttonset-item__title is-last">Last</span> </li> ' +
-                '</ul> ' +
-                '</div>',
+                templateUrl: "features/paginationTemplate.html" ,
                 restrict: 'E',
                 replace: true,
                 scope: {
@@ -43,12 +31,12 @@
                     scope.numPages = 0;
 
                     //Watchers
-                    scope.$watch('viewList.length',function(value){
-                        scope.numPages = (value / scope.limit) >> 0;
-                        if (value % scope.limit !== 0) scope.numPages++;
+                    scope.$watch('viewList',function(value){
+                        scope.numPages = (value.length / scope.limit) >> 0;
+                        if (value.length % scope.limit !== 0) scope.numPages++;
                         scope.pageList.length = scope.numPages;
                         for (var i = 0; i < scope.numPages; ++i) scope.pageList[i] = i;
-                    });
+                    },true);
 
                     //Implementation
                     function goToFirstPage() {
