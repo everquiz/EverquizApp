1416ec72b61c06636bc9c65e3b9fa49326c66f8b
Viktor <valakar@gmail.com>
2015-10-02 03:41:29 +0300

    added three tests for achievment service

--- app/achievements/achievementService.js
+++ app/achievements/achievementService.js
@@ -9,16 +9,26 @@
 
   function achievementService($http, authFactory) {
     var self = this;
+    self.get = get;
+    self.getAll = getAll;
+    self.create = create;
 
     function get(id) {
-        return $http.get('/api/v1/Achievements/' + id
-        //   , {
-        //     headers: {Authorization: 'Bearer ' + authFactory.getToken()}
-        // }
-        )
-        .then(function (res) {
+        return $http.get('/api/v1/Achievements/' + id)
+          .then(function (res) {
             return res.data;
+          });
+    }
+
+    function getAll() {
+      return $http.get('/api/v1/Achievements?select=title,description')
+        .then(function (res) {
+          return res.data;
         });
+    }
+
+    function create (achievment) {
+      return $http.post('/api/v1/Achievements', achievment);
     }
   }
 })();
--- tests/achievements/achievements.service.spec.js
+++ tests/achievements/achievements.service.spec.js
@@ -11,16 +11,96 @@ describe("AchievementService", function() {
         $httpBackend = _$httpBackend_;
     }));
 
-    it('should get achievement from the server', function () {
-        $httpBackend
-            .expectGET('/api/v1/Achievements/', headers)
-            .respond(200);
-        var succeeded;
-        new Person('Ben').create()
-            .then(function () {
-                succeeded = true;
+    it('should get achievements from the server', function () {
+        
+        $httpBackend.whenGET('/api/v1/Achievements?select=title,description').respond({
+            data: [
+                {
+                    title: 'Welcome',
+                    description: 'Welcome to our awesome service!'
+                },
+                {
+                    title: 'First quiz',
+                    description: 'Congratulation on passing your first quiz!'
+                },
+                {
+                    title: 'First note',
+                    description: 'You have created your first note!'
+                }
+            ]
+        });
+
+        var achievements = [];
+        achievementService.getAll()
+            .then(function (res) {
+                achievements = res.data;
             });
+
+        expect(achievements).to.be.empty;
+        expect(achievements).to.be.instanceof(Array);
+        expect(achievements).to.have.length(0);
+
         $httpBackend.flush();
-        expect(succeeded).to.be.true;
+
+        expect(achievements).not.to.be.empty;
+        expect(achievements).to.be.instanceof(Array);
+        expect(achievements).to.have.length(3);
+        expect(achievements).to.deep.include.members([
+            {title: 'Welcome',description: 'Welcome to our awesome service!'},
+            {title: 'First quiz',description: 'Congratulation on passing your first quiz!'},
+            {title: 'First note',description: 'You have created your first note!'}]);
+    });
+
+    it('should post achievement to the server', function () {
+        var achievements = [];
+        $httpBackend.whenPOST('/api/v1/Achievements').respond(function (method, url, data) {
+                achievements.push(angular.fromJson(data));
+                return [200, {}, {}];
+            });
+        expect(achievements).to.be.empty;
+        expect(achievements).to.be.instanceof(Array);
+        expect(achievements).to.have.length(0);
+
+        achievementService.create({title: 'Welcome', description: 'Welcome to our awesome service!'});
+        
+        $httpBackend.flush();
+
+        expect(achievements).not.to.be.empty;
+        expect(achievements).to.have.length(1);
+        expect(achievements).to.deep.include.members([
+            {title: 'Welcome',description: 'Welcome to our awesome service!'}]);
+
+
+    });
+
+    it('should get achievement from the server', function () {
+        var regex = new RegExp('/api/v1/Achievements/([0-9]+)');
+        var achievements = [{_id: '1', title: 'Welcome', description: 'Welcome to our awesome service!'}];
+        var achievement;
+        $httpBackend.whenGET(regex.test('/api/v1/Achievements')).respond(function (method,url,data) {
+                achievements.forEach(function(element, index) {
+                    if(element._id === url.match(regex)[1]) {
+                        achievement = element;
+                        return [202, element, {}];
+                    }
+                });
+                return [404, 'Not found', {}];
+            });
+
+        expect(achievements).not.to.be.empty;
+        expect(achievements).to.have.length(1);
+        expect(achievements).to.deep.include.members([
+            {_id: '1', title: 'Welcome', description: 'Welcome to our awesome service!'}]);
+
+        achievementService.get('1');
+       
+        $httpBackend.flush();
+
+        expect(achievement).not.to.be.empty;
+        expect(achievement).to.be.instanceof(Object);
+        expect(achievement).deep.equal(
+            {_id: '1', title: 'Welcome', description: 'Welcome to our awesome service!'});
+        expect(achievement).not.deep.equal(
+            {_id: '1', title: 'Welcome1', description: 'Welcome to our awesome service!'});
     });
 });
