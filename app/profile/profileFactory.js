c39fdb06fbf2afaf15595ccebea1dc2986a6290e
Viktor <valakar@gmail.com>
2015-10-07 20:29:44 +0300

    alertify

--- app/profile/profileFactory.js
+++ app/profile/profileFactory.js
@@ -5,9 +5,9 @@
         .module('everquizApp')
         .factory('profileFactory', profileFactory);
 
-    profileFactory.$inject = ['$http', 'authFactory', '$q', 'achievementService'];
+    profileFactory.$inject = ['$http', 'authFactory', '$q', 'achievementService', 'ngAlertify'];
 
-    function profileFactory($http, authFactory, $q, achievementService) {
+    function profileFactory($http, authFactory, $q, achievementService, ngAlertify) {
         var profile = {};
         var display = false;
         var observerCallbacks = [];
@@ -153,7 +153,7 @@
             achievementService.get('5614d7cd60a7a12614a331b7');
             for (var i = profile.achievements.length - 1; i >= 0; i--) {
                 if (profile.achievements[i]._id === '5614d7cd60a7a12614a331b7') {
-                    console.log('already achieved!');
+                    // alertify.error("You already have this achievement");
                     return;
                 }
             }
