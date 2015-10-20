c660e3359f0fd8a8c68bdf5205ffddab1d95c8a0
Viktor Novak <valakar@gmail.com>
2015-10-20 16:47:29 +0300

    UserServiceSpec finished

--- app/admin/users/UsersController.js
+++ app/admin/users/UsersController.js
@@ -1,62 +1,46 @@
-(function() {
-  'use strict';
+;(function() {
+    'use strict';
 
-  angular
-      .module('everquizApp')
-      .controller('UsersController', UsersController);
+    angular
+        .module('everquizApp')
+        .controller('UsersController', UsersController);
 
-  UsersController.$inject = ['userService', 'authFactory'];
+    UsersController.$inject = ['userService', 'authFactory'];
 
-  function UsersController(userService, authFactory) {
+    function UsersController(userService, authFactory) {
 
-    var vm = this;
-    vm.users = userService.users;
-    vm.addUser = addUser;
-    vm.editUser = editUser;
-    vm.formTitle = 'Add new user';
-    vm.resetTitle = resetTitle;
-    vm.modalToggle = modalToggle;
+        var vm = this;
+        vm.users = userService.users;
+        vm.addUser = addUser;
+        vm.editUser = editUser;
+        vm.formTitle = 'Add new user';
+        vm.resetTitle = resetTitle;
 
-    function modalToggle() {
-      var modal = document.getElementById('modal');
-      console.log('modal', modal);
-      if (modal.style.opacity == 0) {
-        console.log('opacity 0')
-        modal.style.display = 'block';
-        modal.style.opacity = 1;
-      } else {
-        console.log('opacity 1')
-        modal.style.opacity = 0;
-        modal.style.display = 'none';
-      }
-    };
+        function addUser() {
+            if ((!vm.user.name || vm.user.name === '') || (!vm.user.email || vm.user.email === '')) {
+                return;
+            }
+            if (!vm.user._id || vm.user._id === '') {
+                userService.create(vm.user);
+            } else {
+                userService.update(vm.user);
+            }
+            vm.user = {};
+            vm.formTitle = 'Add new user';
+            vm.user.emailDis = false;
+        };
 
-    function addUser() {
-      if((!vm.user.name || vm.user.name === '')
-          || (!vm.user.email || vm.user.email === '')) 
-        { return; }
-      if (!vm.user._id || vm.user._id === '') {
-        userService.create(vm.user);
-      } else {
-        userService.update(vm.user);
-      }
-      vm.user = {};
-      vm.formTitle = 'Add new user';
-      vm.user.emailDis = false;
-    };
+        function editUser(user) {
+            vm.user = user;
+            vm.formTitle = 'Edit user';
+            vm.user.emailDis = true;
+        };
 
-    function editUser(user) {
-      vm.user = user;
-      vm.formTitle = 'Edit user';
-      vm.user.emailDis = true;
-    };
-
-    function resetTitle () {
-      vm.user = {};
-      vm.formTitle = 'Add new user';
-      vm.user.emailDis = false;
-      vm.modalToggle();
+        function resetTitle() {
+            vm.user = {};
+            vm.formTitle = 'Add new user';
+            vm.user.emailDis = false;
+        }
     }
-  }
 
 })();
--- app/admin/users/userService.js
+++ app/admin/users/userService.js
@@ -1,54 +1,60 @@
-(function() {
-  'use strict'
+;(function() {
+    'use strict';
 
-  angular
-      .module('everquizApp')
-      .service('userService', userService);
+    angular
+        .module('everquizApp')
+        .service('userService', userService);
 
-  userService.$inject = ['$http', 'authFactory'];
+    userService.$inject = ['$http', 'authFactory'];
 
-  function userService($http, authFactory) {
+    function userService($http, authFactory) {
 
-    var self = this;
-    self.users = [];
+        var self = this;
+        self.users = [];
 
-    self.getAll = getAll;
-    self.get = get;
-    self.update = update;
-    self.create = create;
+        self.getAll = getAll;
+        self.get = get;
+        self.update = update;
+        self.create = create;
 
-    function getAll () {
-      return $http.get('/api/v1/Users', {
-        headers: {
-          Authorization: 'Bearer ' + authFactory.getToken()
+        function getAll() {
+            $http.get('/api/v1/Users', {
+                headers: {
+                    Authorization: 'Bearer ' + authFactory.getToken()
+                }
+            }).then(function(res) {
+                angular.copy(res.data, self.users);
+            });
+            return self.users;
+
         }
-      }).then(function (res) {
-        angular.copy(res.data, self.users);
-      });
-      return self.users;
-    };
 
-    function get (id) {
-      return $http.get('/api/v1/Users/' + id + '?populate=notes', {
-          headers: {Authorization: 'Bearer ' + authFactory.getToken()}
-      }).then(function (res) {
-          return res.data;
-      });
-    };
+        function get(id) {
+            return $http.get('/api/v1/Users/' + id + '?populate=notes', {
+                headers: {
+                    Authorization: 'Bearer ' + authFactory.getToken()
+                }
+            }).then(function(res) {
+                return res.data;
+            });
+        }
 
-    function update (user) {
-      return $http.put('/api/v1/Users/' + user._id, user, {
-          headers: {Authorization: 'Bearer ' + authFactory.getToken()}
-      });
-    };
+        function update(user) {
+            return $http.put('/api/v1/Users/' + user._id, user, {
+                headers: {
+                    Authorization: 'Bearer ' + authFactory.getToken()
+                }
+            });
+        }
 
-    function create (user) {
-      return $http.post('/api/v1/Users', user, {
-          headers: {Authorization: 'Bearer ' + authFactory.getToken()}
-      }).success(function(data) {
-        self.users.push(data);
-      })
+        function create(user) {
+            return $http.post('/api/v1/Users', user, {
+                headers: {
+                    Authorization: 'Bearer ' + authFactory.getToken()
+                }
+            }).success(function(data) {
+                self.users.push(data);
+            })
+        }
     };
-  };
-
 })();
