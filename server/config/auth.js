91c8572d8136a99035ec123107caf49643dbf25e
AndrewLolo <andrewloloal@gmail.com>
2015-10-21 10:06:41 +0300

    social registration changed for heroku

--- server/config/auth.js
+++ server/config/auth.js
@@ -7,16 +7,16 @@ module.exports = {
     },
 
     'epamAuth' : {
-        'clientID'      : 'b42f011bad8e54abbe9fff4a4590c0aaecff6cd8fa4e6a848ae5c7838a7043df',
-        'clientSecret'  : '3a667b248743d723c12afea1fc5c411e7258fb9305ecba8a80057e44776495fe',
+        'clientID'      : 'ee8c9c4fc634ec173ea82cc63a47dc91928e2f9d04405e979d21f986d8b4aefc',
+        'clientSecret'  : '2777a9c7e32b89153da98186967f9cb75cf789b9304737ec3761b18bc4373f1f',
         'gitlabURL'     : "https://git.epam.com",
-        'callbackURL'   : 'http://localhost:3000/auth/gitlab/callback'
+        'callbackURL'   : 'https://everquiz.herokuapp.com/auth/gitlab/callback'
     },
 
     'vkAuth' : {
         'clientID'      : '5099697', // your App ID
         'clientSecret'  : 'wvIh5Zk1kvZK2ho1bafC', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/vkontakte/callback'
+        'callbackURL'   : 'https://everquiz.herokuapp.com/auth/vkontakte/callback'
     },
 
     'twitterAuth' : {
@@ -28,7 +28,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '52704386046-917u4gbncibo2jtctjid1mfiuma8enlj.apps.googleusercontent.com',
         'clientSecret'  : 'XvZEZYyKILxR8IVvFNXcUGio',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://everquiz.herokuapp.com/auth/google/callback'
     }
 
 };
