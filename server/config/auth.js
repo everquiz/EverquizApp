e08ba14203e9501dda702605249e5dcd5d0bfcaf
Viktor Novak <valakar@gmail.com>
2015-10-21 13:51:12 +0300

    vk

--- server/config/auth.js
+++ server/config/auth.js
@@ -10,13 +10,13 @@ module.exports = {
         'clientID'      : 'b42f011bad8e54abbe9fff4a4590c0aaecff6cd8fa4e6a848ae5c7838a7043df',
         'clientSecret'  : '3a667b248743d723c12afea1fc5c411e7258fb9305ecba8a80057e44776495fe',
         'gitlabURL'     : "https://git.epam.com",
-        'callbackURL'   : 'http://everquiz.herokuapp.com/auth/gitlab/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/gitlab/callback'
     },
 
     'vkAuth' : {
         'clientID'      : '5099697', // your App ID
         'clientSecret'  : 'wvIh5Zk1kvZK2ho1bafC', // your App Secret
-        'callbackURL'   : 'http://everquiz.herokuapp.com/auth/vkontakte/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/vkontakte/callback'
     },
 
     'twitterAuth' : {
@@ -28,7 +28,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '52704386046-917u4gbncibo2jtctjid1mfiuma8enlj.apps.googleusercontent.com',
         'clientSecret'  : 'XvZEZYyKILxR8IVvFNXcUGio',
-        'callbackURL'   : 'https://everquiz.herokuapp.com/auth/google/callback'
+        'callbackURL'   : 'https://localhost:3000/auth/google/callback'
     }
 
 };
